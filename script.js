0c63fb3261f2da467a138c3afd1aa56b7e0ca5d8
NahomAzmach <azmachn@wwu.edu>
2023-07-05 16:32:59 -0700

    Created the original interface

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+function searchListings() {
+    // Implement search functionality
+    // Retrieve search input value
+    const searchInput = document.getElementById('searchInput').value;
+    
+    // Make API request or process scraped data based on the search input
+    // Update the listings dynamically on the page
+    // ...
+  }
+  
