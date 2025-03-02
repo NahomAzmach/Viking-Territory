d80da75a8b4d951703cf84f7c37764af38b8b704
Nahom Azmach <121968256+NahomAzmach@users.noreply.github.com>
2025-03-01 19:40:54 -0800

    Add files via upload

---
+++ rental-properties-app/client/src/ReactJunk/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropertyList from './components/PropertyList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Viking Territory</h1>
+      </header>
+      <PropertyList />
+    </div>
+  );
+}
+
+export default App;
